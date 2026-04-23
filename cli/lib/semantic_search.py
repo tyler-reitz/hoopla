5e8bdb447a296d4f9e0e709a828929b1230ce1d5
tyler <tyler.reitz@protonmail.com>
2026-04-23 14:19:46 -0700

    CH4;L11

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,13 +1,54 @@
+import json
+import os
+
+import numpy as np
+
 from sentence_transformers import SentenceTransformer
+
+
+title_desc = lambda mov: f"{mov['title']}: {mov['description']}"
 
 class SemanticSearch:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
 
     def generate_embeddings(self, text):
         if not text.strip():
             raise ValueError('Text cannot be empty or whitespace only')
         return self.model.encode([text])[0]
+
+    def build_embeddings(self, documents):
+        self.documents = documents
+        for doc in documents:
+            self.document_map[doc['id']]=doc
+        self.embeddings = self.model.encode(
+                list(map(title_desc, documents)), show_progress_bar=True)
+        np.save('cache/movie_embeddings.npy', self.embeddings)
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        for doc in documents:
+            self.document_map[doc['id']]=doc
+        if os.path.exists('cache/movie_embeddings.npy'):
+            self.embeddings = np.asarray(np.load('cache/movie_embeddings.npy'))
+            if len(self.embeddings) == len(self.documents):
+                return self.embeddings
+        else:
+            return self.build_embeddings(documents)
+
+
+def verify_embeddings():
+    print("Verifying embeddings")
+    ss = SemanticSearch()
+    with open("data/movies.json") as f:
+        results = json.load(f)
+    embeddings = ss.load_or_create_embeddings(results['movies'])
+    print(f"Number of docs: {len(results['movies'])}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
 
 def embed_text(text):
     ss = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,20 +2,23 @@
 
 import argparse
 
-from lib.semantic_search import verify_model, embed_text
+from lib.semantic_search import verify_model, embed_text, verify_embeddings
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available command")
 
-    subparsers.add_parser("verify", help="Verify Semantic Search Model")
+    subparsers.add_parser("verify", help="Verify Semantic Search Model is loaded")
+    subparsers.add_parser("verify_embeddings", help="Verify Semantic Search embeddings")
 
-    embed_parser = subparsers.add_parser("embed_text", help="Embed Semantic Search Model")
+    embed_parser = subparsers.add_parser("embed_text", help="Generate an embedding for a single text")
     embed_parser.add_argument("text", type=str, help="Text to embed")
 
     args = parser.parse_args()
 
     match args.command:
+        case "verify_embeddings":
+            verify_embeddings()
         case "embed_text":
             embed_text(args.text)
         case "verify":
